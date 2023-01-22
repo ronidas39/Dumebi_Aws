6abb0670d128491052fd18164b2287fbe2688f4b
ronidas39 <ronidas041987@gmail.com>
2023-01-22 19:57:40 +0530

    class 22 nd jan

--- IAM/create_from_file.py
+++ IAM/create_from_file.py
@@ -1,10 +1,14 @@
-# you have a list of usernames as below
-import boto3
 import io
+import boto3
 client=boto3.client("iam")
-usernames=["roni","john","mark"]
 
-for username in usernames:
+with io.open("input.csv","r",encoding="utf-8")as f1:
+    data=f1.read()
+    f1.close()
+lines=data.split("\n")[1:]
+for line in lines:
+    username=line.split(",")[0]
+    password=line.split(",")[1]
     client.create_user(UserName=username)
     client.create_login_profile(UserName=username,Password="Welcome@1234",PasswordResetRequired=False)
     response=client.create_access_key(UserName=username)
@@ -15,5 +19,3 @@ for username in usernames:
     with io.open(username+".txt","w",encoding="utf-8")as f1:
         f1.write(access_key+","+secret_access_key)
         f1.close()
-    
-#use the python list concept to create all 3 iam users together using python code
